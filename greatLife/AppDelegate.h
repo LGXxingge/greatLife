3cf185747791af45af118f5f8ea08bf95bc0c3e3
兴哥哥 <LGX@xinggegedemacbook-pro.local>
2016-06-21 22:24:21 +0800

    Initial Commit

---
+++ greatLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  greatLife
+//
+//  Created by 兴哥哥 on 16/6/21.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ greatLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  greatLife
+//
+//  Created by 兴哥哥 on 16/6/21.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
